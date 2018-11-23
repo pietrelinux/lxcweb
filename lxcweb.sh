7b1f854b1ce671b9a2e79c41d3884b180ae432dd
pietre.linux <pietre.linux@gmail.com>
2018-11-23 04:01:29 +0100

    Update lxcweb.sh

--- lxcweb.sh
+++ lxcweb.sh
@@ -3,6 +3,7 @@ locale-gen es_ES.UTF-8
 export LC_ALL="es_ES.UTF-8"
 update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
 dpkg-reconfigure locales
+curl https://packagecloud.io/gpg.key | apt-key add -
 #Añadimos un repositorio ppa e introducimos lineas:
 touch /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
 echo "deb https://packagecloud.io/claudyus/LXC-Web-Panel/ubuntu/ xenial main" > /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
