7553af8beaa3fba82322da1736b08fb7119352f9
pietre.linux <pietre.linux@gmail.com>
2018-11-23 03:54:23 +0100

    Update lxcweb.sh

--- lxcweb.sh
+++ lxcweb.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
-
-#Actualizamos los repositorios y los paquetes
-apt-get -y update
-apt-get -y upgrade
-
-#Instalamos curl y añadimos la clave publica
-apt-get -y install curl
-curl https://packagecloud.io/gpg.key | apt-key add -
-apt-get -y update
-
-#Instalamos el siguiente paquete para verificar repositorios
-apt-get -y install debian-archive-keyring
-
-#Instalamos para poder utilizar repositorios ubicamos en https, posiblemente
-#ya estará instalado
-
-apt-get install -y apt-transport-https
-
 #Añadimos un repositorio ppa e introducimos lineas:
 touch /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
-echo "deb https://packagecloud.io/claudyus/LXC-Web-Panel/ubuntu/ trusty main" > /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
-echo "deb-src https://packagecloud.io/claudyus/LXC-Web-Panel/ubuntu/ trusty main" > /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
-
-#Actualizamos los repositorios e instalamos lwp
+echo "deb https://packagecloud.io/claudyus/LXC-Web-Panel/ubuntu/ xenial main" > /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
+echo "deb-src https://packagecloud.io/claudyus/LXC-Web-Panel/ubuntu/ xenial main" > /etc/apt/sources.list.d/claudyus_LXC-Web-Panel.list
 apt-get -y update
-apt-get -y install lwp
+apt-get -y upgrade -y
 
-#Copiamos el fichero de configuracion
+apt-get install lxc install curl debian-archive-keyring  apt-transport-https lwp -y
+
 cp /etc/lwp/lwp.example.conf /etc/lwp/lwp.conf
 
 #Reiniciamos el servicio:
 service lwp restart
 
-echo "Listo ya tienes instalado lwp"
